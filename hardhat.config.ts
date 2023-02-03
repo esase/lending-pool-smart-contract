4e87f98bb239a7ae007c139aa52a07b9e187910c
Alex Ermashev <alex.ermashev@carrentalgateway.com>
2023-02-03 15:09:46 +0600

    fix

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: '0.8.17',
+    settings: {
+        optimizer: {
+            enabled: true,
+            runs: 1000000,
+        },
+    },
+},
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    coinmarketcap: '8fddfefc-e706-4355-8c1f-0d4b907f440e',
+  },
 };
 
 export default config;
